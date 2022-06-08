35e88c742abd3f9b32fb7da6520c104962086d7c
kylem <kylem@kylem.com>
2022-06-07 23:53:44 -0400

    Changing application name and icon, modifying xml and java files with comments to include my name and student information

--- KyleAssign22/src/main/java/kyle/menegotti/n01462135/MenegottiActivity.java
+++ KyleAssign22/src/main/java/kyle/menegotti/n01462135/MenegottiActivity.java
@@ -1,10 +1,12 @@
+/* Kyle Menegotti, n01462135, Section: RNB */
+
 package kyle.menegotti.n01462135;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MenegottiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
