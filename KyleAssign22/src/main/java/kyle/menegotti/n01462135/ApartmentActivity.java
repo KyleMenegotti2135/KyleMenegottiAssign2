bb5ebdc1b3a906bab5956e2734350cf84b8400c5
KyleMenegotti2135 <105381987+KyleMenegotti2135@users.noreply.github.com>
2022-06-10 00:25:48 -0400

    added activity for apartments page, apt info, uploaded apartment images. Created checkboxes. Created/implemented scroll wheel/linear layout, so user can view all apartment options. Created button once user selects apartment to proceed to next page. Finished XML layout for apartments page. Finished logic for apartment activity, user selection of apt now brings user to an empty payment page

--- KyleAssign22/src/main/java/kyle/menegotti/n01462135/ApartmentActivity.java
+++ KyleAssign22/src/main/java/kyle/menegotti/n01462135/ApartmentActivity.java
@@ -1,14 +1,46 @@
+// Kyle Menegotti, N01462135, Section RNB
+
 package kyle.menegotti.n01462135;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
-public class ApartmentActivity extends AppCompatActivity {
+public class ApartmentActivity extends MenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apartment);
+
+
+        CheckBox OptionOne = findViewById(R.id.KyleCheckBoxOne);
+        CheckBox OptionTwo = findViewById(R.id.KyleCheckBoxTwo);
+        CheckBox OptionThree = findViewById(R.id.KyleCheckBoxThree);
+        Button btn = findViewById(R.id.KyleButton1);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (OptionOne.isChecked() || OptionTwo.isChecked() || OptionThree.isChecked())
+                {
+                    Intent i = new Intent(ApartmentActivity.this, PaymentActivity.class);
+                    startActivity(i);
+                }
+
+                else
+                {
+                    Toast toast=Toast.makeText(getApplicationContext(),"Please choose an option.",Toast.LENGTH_SHORT);
+                    toast.setMargin(50,50);
+                    toast.show();
+                }
+            }
+        });
     }
 }
