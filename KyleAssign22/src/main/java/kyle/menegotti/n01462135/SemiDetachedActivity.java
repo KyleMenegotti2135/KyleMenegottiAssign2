e85106c97ac6dc504795d6d5b198e241f48ab93e
KyleMenegotti2135 <105381987+KyleMenegotti2135@users.noreply.github.com>
2022-06-10 02:41:39 -0400

    Final Commit. Made Semi_detached activity and java functions to make them work OnClick and deliver user to payment screen. Uploaded images, and followed similar scrollview/linear layout method. I wish I used check boxes at the start, when the user is selecting what type of house they want. This would have made it so the user can unselect, without having to return to the home page every time(a small inconvience)

--- KyleAssign22/src/main/java/kyle/menegotti/n01462135/SemiDetachedActivity.java
+++ KyleAssign22/src/main/java/kyle/menegotti/n01462135/SemiDetachedActivity.java
@@ -1,14 +1,48 @@
+//Kyle Menegotti, N01462135, Section RNB
+
 package kyle.menegotti.n01462135;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
-public class SemiDetachedActivity extends AppCompatActivity {
+public class SemiDetachedActivity extends MenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_semi_detached);
+
+
+        CheckBox OptionOne = findViewById(R.id.KyleCheckBoxOne);
+        CheckBox OptionTwo = findViewById(R.id.KyleCheckBoxTwo);
+        CheckBox OptionThree = findViewById(R.id.KyleCheckBoxThree);
+        Button btn = findViewById(R.id.KyleButtonNew);
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (OptionOne.isChecked() || OptionTwo.isChecked() || OptionThree.isChecked())
+                {
+                    Intent i = new Intent(SemiDetachedActivity.this, PaymentActivity.class);
+                    startActivity(i);
+                }
+
+                else
+                {
+                    Toast toast=Toast.makeText(getApplicationContext(),"Select an Option",Toast.LENGTH_SHORT);
+                    toast.setMargin(50,50);
+                    toast.show();
+                }
+            }
+        });
     }
 }
